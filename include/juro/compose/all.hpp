92ef8cd06fa5a85d5d2f0175ba3d1e0f8ee253e6
André Medeiros <andsmedeiros@gmail.com>
2023-05-28 15:53:21 -0300

    Adds a void template specialisation to `all()`
    - Handles cases when all provided promises' types are void with a simpler approach
    - Segregates some non-templated code in its own compilation unit

--- include/juro/compose/all.hpp
+++ include/juro/compose/all.hpp
@@ -24,26 +24,26 @@ class all_coordinator : public std::enable_shared_from_this<all_coordinator<T_va
     promise_ptr<result_type> promise;
 
 public:
-    all_coordinator(promise_ptr<result_type> promise) : 
-        promise { promise } 
-        {  }
+    all_coordinator(const promise_ptr<result_type> &promise) :
+        promise { promise }
+    {  }
 
     template<std::size_t ...Indices>
-    void attach(std::index_sequence<Indices...>, std::tuple<promise_ptr<T_values> &...> &&promises) {
+    void attach(std::index_sequence<Indices...>, std::tuple<const promise_ptr<T_values> &...> &&promises) {
         ([&, this] (auto &promise, auto &slot) {
-            using promise_type = 
+            using promise_type =
                 typename std::remove_reference_t<decltype(promise)>::element_type;
             if constexpr(promise_type::is_void) {
                 promise->then(
-                    [&, guard = this->shared_from_this()] { 
-                        on_resolve(void_type {}, slot); 
+                    [this, &slot, guard = this->shared_from_this()] {
+                        on_resolve(void_type {}, slot);
                     },
                     [&] (std::exception_ptr &error) { on_reject(error); }
                 );
             } else {
                 promise->then(
-                    [&, guard = this->shared_from_this()] (auto &value) { 
-                        on_resolve(value, slot); 
+                    [this, &slot, guard = this->shared_from_this()] (auto &value) {
+                        on_resolve(value, slot);
                     },
                     [&] (std::exception_ptr &error) { on_reject(error); }
                 );
@@ -54,7 +54,7 @@ public:
     template<class T_value>
     void on_resolve(T_value &&value, std::optional<std::remove_reference_t<T_value>> &slot) {
         slot = std::forward<T_value>(value);
-        
+
         if(--counter == 0 && promise->is_pending()) {
             promise->resolve(std::apply([] (auto &...values) {
                 return std::make_tuple(std::move(values.value())...);
@@ -69,15 +69,26 @@ public:
     }
 };
 
+void void_settler(const promise_ptr<void> &promise, const promise_ptr<void> &all_promise, const std::shared_ptr<std::size_t> &counter);
+
 template<class ...T_values>
-auto all(promise_ptr<T_values> ...promises) {
-    return make_promise<all_result<T_values...>>([&] (auto &all_promise) {
-        auto coordinator = std::make_shared<all_coordinator<T_values...>>(all_promise);
-        coordinator->attach(
-            std::index_sequence_for<T_values...>(), 
-            std::forward_as_tuple(promises...)
-        );
-    });
+auto all(const promise_ptr<T_values> &...promises) {
+    if constexpr(std::conjunction_v<std::is_void<T_values>...>) {
+        return make_promise<void>([&] (const auto &all_promise) {
+            auto counter = std::make_shared<std::size_t>(sizeof...(T_values));
+            for(const auto &promise : std::array<std::reference_wrapper<const promise_ptr<void>>, sizeof...(T_values)> { promises... }) {
+                void_settler(promise, all_promise, counter);
+            }
+        });
+    } else {
+        return make_promise<all_result<T_values...>>([&] (const auto &all_promise) {
+            auto coordinator = std::make_shared<all_coordinator<T_values...>>(all_promise);
+            coordinator->attach(
+                std::index_sequence_for<T_values...>(),
+                std::forward_as_tuple(promises...)
+            );
+        });
+    }
 }
 
 } /* namespace juro::compose */
